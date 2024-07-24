b88fefd0a8df3d3fa96cb4938fce34c87bad0c18
Namratha <namrathanjjadhav@gmail.com>
2024-07-24 19:09:48 +0530

    pratice

---
+++ kt11.py
@@ -0,0 +1,27 @@
+#membership operator
+list = "hello how are you"
+print('h' in list)
+print('b' in list)
+print('b' not in list)
+
+valid_username = ["admin", "user1","user2"]
+username = input("eneter your username:")
+if username in valid_username:
+     print("welcome back", username)
+else:
+    print("invalid username. please try again.")
+
+ #clear
+    numbers = [3,5,6,8,9,11,14]
+print(numbers)
+numbers.clear()
+print(numbers)
+
+#delete 
+my_list = [1,2,3]
+del my_list
+
+
+
+
+
